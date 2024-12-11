dae4f3c3c23ba452cff6e42ae3952867ffbb4efe
borsuczyna <libxxerty@gmail.com>
2024-12-11 18:43:18 +0100

    change tabs to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,20 @@ import * as vscode from 'vscode';
 import { ResourceTreeProvider } from './resources/view-provider';
 
 function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from your extension!');
-	});
+    let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+        vscode.window.showInformationMessage('Hello World from your extension!');
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 
-	const resourceTreeProvider = new ResourceTreeProvider();
+    const resourceTreeProvider = new ResourceTreeProvider();
     vscode.window.registerTreeDataProvider('exportsView', resourceTreeProvider);
 
-	// Watch for changes in the workspace
-	const watcher = vscode.workspace.createFileSystemWatcher('**/*', false, false, false);
-	watcher.onDidCreate(() => resourceTreeProvider.refresh());
-	watcher.onDidDelete(() => resourceTreeProvider.refresh());
-	watcher.onDidChange(() => resourceTreeProvider.refresh());
+    // Watch for changes in the workspace
+    const watcher = vscode.workspace.createFileSystemWatcher('**/*', false, false, false);
+    watcher.onDidCreate(() => resourceTreeProvider.refresh());
+    watcher.onDidDelete(() => resourceTreeProvider.refresh());
+    watcher.onDidChange(() => resourceTreeProvider.refresh());
 }
 
 function deactivate() {}
